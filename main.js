7713960d90c1d6477b302769deecdfd016100525
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-07 19:15:45 -0400

    randomly selecting an event

---
+++ main.js
@@ -0,0 +1,12 @@
+// Randomly selecting an event
+const getRandEvent = () => {
+    const random = Math.floor(Math.random() * 3);
+
+    if (random === 0) {
+        return 'Marathon';
+    } else if (random === 1) {
+        return 'Triathlon';
+    } else if (random === 2) {
+        return 'Pentathlon';
+    }
+};
